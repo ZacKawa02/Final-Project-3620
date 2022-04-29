754a176bf1958604aef7242b777cdce8f240d5db
Zachary Kawa <zac.kawa@uleth.ca>
2022-04-28 19:33:12 -0600

    fixed it

--- src/SlidingSolver.cpp
+++ src/SlidingSolver.cpp
@@ -24,7 +24,6 @@
     //for each Board_Tile in the list of next possible configurations, push
     //each to the heap using it's MD as a key
     for (unsigned int i = 0; i < list.size(); i++) {
-      key = list.at(i);
-      tileQueue.push();
+      tileQueue.push(list.at(i));
     }
   }
